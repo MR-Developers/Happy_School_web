3e68908b3bdc65cfbf942aa8e1d30f9944ec81bf
Rajabrahmam-Thanniru <raja9392t@gmail.com>
2025-06-30 08:55:27 +0530

    coverted to ts

--- backend/convert.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const json = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf-8'));
-console.log(JSON.stringify(json));
--- backend/routes/RaiseTicketRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {RaiseTicketController} = require("../controllers/RaiseTicketController");
-
-router.post("/raiseTicket/:email", RaiseTicketController);
-
-module.exports = router;
