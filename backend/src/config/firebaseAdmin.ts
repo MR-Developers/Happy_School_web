c729ac207e851037048282608e538bdcc24beef9
Rajabrahmam-Thanniru <raja9392t@gmail.com>
2025-07-07 23:28:37 +0530

    try2

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json'; // Adjust the path as needed
+import firebaseServiceAccount from '../../serviceAccountKey.json'; // Adjust the path as needed
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(firebaseServiceAccount as admin.ServiceAccount),
     // Optionally include databaseURL if you're using Realtime DB
     databaseURL: 'https://your-project-id.firebaseio.com',
   });
