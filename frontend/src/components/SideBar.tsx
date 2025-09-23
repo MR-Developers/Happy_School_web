befc3f17489c11851451bf0778175245131fc3a0
Rajabrahmam-Thanniru <raja9392t@gmail.com>
2025-09-23 21:41:47 +0530

    changed the name of the reports

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -31,7 +31,7 @@ function SideBar() {
       path: "/one-one-sessions",
     },
     {
-      name: "Survey  Report",
+      name: "Survey Report",
       icon: faChartLine,
       path: "/reports",
     },
